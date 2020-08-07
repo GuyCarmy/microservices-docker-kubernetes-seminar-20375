5d74dfaa8d98b0cecd5303eac8d54f72ae4dd8ba
guycarmy <guycarmy@gmail.com>
2020-08-07 15:39:28 +0300

    Add full name handler

--- microservice-first-name/main.go
+++ microservice-first-name/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 const FIRST_NAME = "Guy"
@@ -11,6 +12,7 @@ func main() {
 	http.HandleFunc("/_ready", readinessHandler)
 	http.HandleFunc("/_alive", livenessHandler)
 	http.HandleFunc("/get_first_name", firstNameHandler)
+	http.HandleFunc("/get_full_name", fullNameHandler)
 	fmt.Println("http server listening on port: 8090")
 	http.ListenAndServe(":8090", nil)
 }
@@ -30,5 +32,17 @@ func livenessHandler(w http.ResponseWriter, req *http.Request) {
 func firstNameHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Handled firstName request")
 	fmt.Fprintln(w, FIRST_NAME)
+	return
+}
+
+func fullNameHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Handled fullName request")
+	last_name, err := http.Get(os.Getenv("MICROSERVICE_LAST_NAME"))
+	if err != nil {
+		fmt.Println("Existed full name handler with error ", err.Error())
+		http.Error(w, "full name handler failed", 500)
+		return
+	}
+	fmt.Fprintln(w, "My full name is ", FIRST_NAME, last_name)
 	return
 }
